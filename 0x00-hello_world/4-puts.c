f731cce9c68c04b363344b32b13db23ae083ee6c
Mohamed2742 <mohamedahmed2742@gmail.com>
2022-07-03 10:46:31 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+ {
+	 char test_str[] = "\"programming is like building a multilingual puzzle";
+
+	 puts(test_str);
+	 return (0);
+ }
