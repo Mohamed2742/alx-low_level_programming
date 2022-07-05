1417db095664b2fd7dd067b0b7dced75ea7109b6
Mohamed2742 <mohamedahmed2742@gmail.com>
2022-07-05 11:27:33 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  *main -Entry point
  * 
- * Return: Anything  (Success)
+ * Return: Always 0  (Success)
  */
 int main(void)
  {
 	 puts( "\"programming is like building a multilingual puzzle");
- 
+         (Anything);
           return (0);
  }
