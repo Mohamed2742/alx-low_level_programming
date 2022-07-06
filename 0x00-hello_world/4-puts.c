a295970ee03bdac107c14e3f03e8ca0b06b631b3
Mohamed2742 <mohamedahmed2742@gmail.com>
2022-07-06 03:43:38 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,8 @@
  */
 int main(void)
  {
-	 puts( "\"programming is like building a multilingual puzzle \n");
+	 puts( "\"programming is like building a multilingual puzzle ");
+	 echo -e "Anything\n"
        
           return (0);
  }
