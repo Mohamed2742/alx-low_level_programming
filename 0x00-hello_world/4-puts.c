b8c11a6b70a4c2af502968030ba3d54906a0f6a9
Mohamed2742 <mohamedahmed2742@gmail.com>
2022-07-07 09:26:34 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this users program prints exactly the string
- * followed by a new line.
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
